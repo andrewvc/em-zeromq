6fa70d451c0d20b7b481a9cf409fdeffa97db880
Julien Ammous <j.ammous@gmail.com>
2011-02-12 23:40:32 +0100

    simple example updated

--- example/simple.rb
+++ example/simple.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'em-zeromq'
     
 Thread.abort_on_exception = true
@@ -7,43 +8,28 @@ class EMTestPullHandler
   attr_reader :received
   def on_readable(socket, messages)
     messages.each do |m|
-      puts m.copy_out_string
+      print '_'
+      # puts m.copy_out_string
     end
-  end
-end
-class EMTestPushHandler
-  attr_accessor :connection
-  attr_reader   :queue
-  
-  def initialize
-    @queue = []
-  end
-   
-  def on_writable(socket)
-    @queue.each do |item|
-      socket.send_string item, ZMQ::NOBLOCK
-    end
-    connection.deregister_writable
   end
 end
 
 EM.run do
-  ZCTX  = ZMQ::Context.new 1
-  send_queue = []
-
-
-  push_handler = EMTestPushHandler.new
-  push = EM::ZeroMQ.create ZCTX, ZMQ::PUSH, :bind, 'tcp://127.0.0.1:2091', push_handler
-  push_handler.connection = push
+  reactor = EM::ZeroMQ::Reactor.new(1)
   
+  push_socket = reactor.bind( ZMQ::PUSH, 'tcp://127.0.0.1:2091')
+  pull_socket = reactor.connect( ZMQ::PULL, 'tcp://127.0.0.1:2091', EMTestPullHandler.new)
   
-  pull = EM::ZeroMQ.create ZCTX, ZMQ::PULL, :connect, 'tcp://127.0.0.1:2091', EMTestPullHandler.new
-  pull.register_readable
-      
-  EM::PeriodicTimer.new(1) do
-    puts '.'
-    push_handler.queue << "Test message #{Time.now}"
-    push.register_writable
+  n = 0
+  
+  push_socket.hwm = 40
+  
+  puts push_socket.hwm
+  puts pull_socket.hwm
+  
+  EM::PeriodicTimer.new(0.01) do
+    print '.'
+    push_socket.send_string("_#{n += 1}_")
   end
 end
 
