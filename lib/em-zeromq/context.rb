67385c9a400fb4f5e8f076ab32740a1ceab790e8
Stefan Kaes <skaes@railsexpress.de>
2011-07-03 10:14:49 +0200

    API compatibility with released vrsion of gem

--- lib/em-zeromq/context.rb
+++ lib/em-zeromq/context.rb
@@ -18,23 +18,19 @@ module EventMachine
         end
       end
 
-      def bind(socket, address, handler = nil, opts = {})
+      def bind(socket_or_type, address, handler = nil, opts = {})
+        socket, type = create(socket_or_type, opts)
         socket.bind(address)
-        watch(socket, address, handler, opts)
+        watch(socket, type, address, handler, opts)
       end
 
-      def connect(socket, address, handler = nil, opts = {})
+      def connect(socket_or_type, address, handler = nil, opts = {})
+        socket, type = create(socket_or_type, opts)
         socket.connect(address)
-        watch(socket, address, handler, opts)
+        watch(socket, type, address, handler, opts)
       end
 
-      def socket(socket_type)
-        socket_type = find_type(socket_type)
-        @context.socket(socket_type)
-      end
-
-      def watch(socket, address, handler, opts = {})
-        socket_type = socket.getsockopt(ZMQ::TYPE)
+      def watch(socket, socket_type, address, handler, opts = {})
         fd = socket.getsockopt(ZMQ::FD)
         conn = EM.watch(fd, EventMachine::ZeroMQ::Connection, socket, socket_type, address, handler)
 
@@ -50,6 +46,28 @@ module EventMachine
       end
 
     private
+
+      def create(socket_or_type, opts = {})
+        if socket_or_type.is_a?(ZMQ::Socket)
+          type = socket.getsockopt(ZMQ::TYPE)
+          socket = socket_or_type
+        else
+          type = find_type(socket_or_type)
+          socket = @context.socket(type)
+        end
+
+        ident = opts.delete(:identity)
+        if ident
+          socket.setsockopt(ZMQ::IDENTITY, ident)
+        end
+
+        unless opts.empty?
+          raise "unknown keys: #{opts.keys.join(', ')}"
+        end
+
+        [socket, type]
+      end
+
       def find_type(type)
         if type.is_a?(Symbol) or type.is_a?(String)
           ZMQ.const_get(type.to_s.upcase)
